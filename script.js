bcda2fe5f617b294eda361daa1c3f3787379ce06
codepapa360 <codepapa360@gmail.com>
2023-04-02 20:41:36 +0600

    Another retouching

--- script.js
+++ script.js
@@ -4,17 +4,16 @@ const navBar = document.querySelector(".nav-items");
 const overlay = document.querySelector(".overlay");
 
 btnMenu.addEventListener("click", function () {
-    navBar.style.transform = "translateX(0)";
-    overlay.style.display = "block";
+  navBar.style.transform = "translateX(0)";
+  overlay.style.display = "block";
 });
 
-btnClose.addEventListener("click", function() {
-    navBar.style.transform = "translateX(100vw)";
-    overlay.style.display = "none"
-
+btnClose.addEventListener("click", function () {
+  navBar.style.transform = "translateX(100vw)";
+  overlay.style.display = "none";
 });
 
-overlay.addEventListener("click", function() {
-    navBar.style.transform = "translateX(100vw)";
-    overlay.style.display = "none"
+overlay.addEventListener("click", function () {
+  navBar.style.transform = "translateX(100vw)";
+  overlay.style.display = "none";
 });
