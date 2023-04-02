5162ef44b3604d30779dfc5f6e586b9830e63efe
codepapa360 <codepapa360@gmail.com>
2023-04-02 22:06:07 +0600

    Added dark mode feature

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const btnMenu = document.querySelector(".btn-menu");
 const btnClose = document.querySelector(".btn-close");
 const navBar = document.querySelector(".nav-items");
 const overlay = document.querySelector(".overlay");
+// const toggleButton = document.querySelector(".dark-mode-toggle");
+// const body = document.body;
 
 btnMenu.addEventListener("click", function () {
   navBar.style.transform = "translateX(0)";
@@ -16,4 +18,15 @@ btnClose.addEventListener("click", function () {
 overlay.addEventListener("click", function () {
   navBar.style.transform = "translateX(100vw)";
   overlay.style.display = "none";
+});
+
+// toggleButton.addEventListener("click", function () {
+//   body.classList.toggle("dark-mode");
+// });
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const root = document.documentElement;
+
+darkModeToggle.addEventListener("click", () => {
+  root.classList.toggle("dark-mode");
 });
